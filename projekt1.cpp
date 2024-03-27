5dccdfb057e09e735b4a3d488dc40a26a257c980
kacpersz02 <siluuux@gmail.com>
2024-03-27 14:38:35 +0100

    dodano funkcje silnia

--- projekt1.cpp
+++ projekt1.cpp
@@ -2,6 +2,14 @@
 
 using namespace std;
 
+int silnia(int n)
+{
+	if (n==0 || n==1)
+		return 1;
+	else
+		return n * silnia(n-1);
+}
+
 int main() {
 	int a, b;
 	cout << "Podaj liczbe calkowita ";
